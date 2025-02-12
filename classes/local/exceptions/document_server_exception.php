b439aad89e9048ed4f4cc25b2116991ff4d8cf7b
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-02-12 18:22:17 +0500

    create general document server exception

---
+++ classes/local/exceptions/document_server_exception.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Document server exception.
+ *
+ * @package    mod_onlyofficeeditor
+ * @copyright  2025 Ascensio System SIA <integration@onlyoffice.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_onlyofficeeditor\local\exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * Exception indicating document server errors.
+ *
+ * @package    mod_onlyofficeeditor
+ * @copyright  2025 Ascensio System SIA <integration@onlyoffice.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class document_server_exception extends Exception {
+    /**
+     * Constructor
+     *
+     * @param string $message The error message
+     * @param int $code The error code
+     * @param Throwable|null $previous Previous throwable
+     */
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
