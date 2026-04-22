5212c646b3f3c35cf44069beb7f408591f233fa9
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-04-22 14:44:13 +0500

    fix(editor): remove gap between open drawer and fullscreen container

--- amd/src/editor.js
+++ amd/src/editor.js
@@ -42,6 +42,17 @@ define(['jquery'], function($) {
             var navRightButton = $('.drawertoggle')[2];
             var editorContainer = $('.onlyofficeeditor-container')[0];
 
+            var page = document.getElementById('page');
+            var syncDrawerMargins = function() {
+                const leftDrawerOpened = page && page.classList.contains('show-drawer-left');
+                const rightDrawerOpened = page && page.classList.contains('show-drawer-right');
+                editorContainer.style.marginLeft = leftDrawerOpened ? '0' : '';
+                editorContainer.style.marginRight = rightDrawerOpened ? '0' : '';
+            };
+            if (page) {
+                new MutationObserver(syncDrawerMargins).observe(page, {attributes: true, attributeFilter: ['class']});
+            }
+
             $.when(enterFullScreenText).done(function(localized) {
                 enterFullScreenText = localized;
                 var enterButton = document.createElement('button');
