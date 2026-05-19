da829595d58a45391c8142d612528ad433062d9f
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-05-19 16:10:10 +0500

    fix: new user-voice url

--- settings.php
+++ settings.php
@@ -35,7 +35,7 @@ if ($ADMIN->fulltree) {
     $customizationapiurl =
     'https://api.onlyoffice.com/docs/docs-api/usage-api/config/editor/customization/customization-standard-branding';
     $helpcentermoodleurl = 'https://helpcenter.onlyoffice.com/integration/moodle.aspx';
-    $feedbackurl = 'https://feedback.onlyoffice.com/forums/966080-your-voice-matters?category_id=519288';
+    $feedbackurl = 'https://community.onlyoffice.com/c/suggestions/integrations/56';
     $defaultjwtheader = 'Authorization';
     $bannerdata = [
         'title' => get_string('banner_title', 'onlyofficeeditor'),
